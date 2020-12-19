62b709e20732e05f308f77d69306382fcbc2299d
Amit Raj <amitraj9046@gmail.com>
2020-12-19 21:06:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,58 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-const App = () => {
+const App = ({ slides }) => {
+ const [index, setIndex] = useState(0);
+
+  let title = slides[index].title;
+  let text = slides[index].text;
+
+  function handleClick(action) {
+    //console.log(action);
+    if (action === "prev") {
+      //if (show.index === 0) return;
+
+      let newIndex = index - 1;
+      setIndex(newIndex);
+    } else if (action === "next") {
+      //if (show.index === slides.length - 1) return;
+
+      let newIndex = index + 1;
+      setIndex(newIndex);
+    } else if (action === "restart") {
+      setIndex(0);
+    }
+  }
+
   return (
-    <></>
-  )
-}
+    <>
+      <button
+        onClick={() => handleClick("restart")}
+        data-testid="button-restart"
+        disabled={index === 0 ? true : false}
+      >
+        Restart
+      </button>
+      <button
+        onClick={() => handleClick("prev")}
+        data-testid="button-prev"
+        disabled={index === 0 ? true : false}
+      >
+        Prev
+      </button>
+      <button
+        onClick={() => handleClick("next")}
+        data-testid="button-next"
+        disabled={index === slides.length - 1 ? true : false}
+      >
+        Next
+      </button>
+
+      <h1 data-testid="title">{title}</h1>
+      <p data-testid="text">{text}</p>
+    </>
+  );
+};
 
 
 export default App;
